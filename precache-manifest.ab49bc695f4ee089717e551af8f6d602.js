ccad74e6b787d39dc6b7e89251620fac6836e702
Scott Pribe <Scottpribe@gmail.com>
2020-08-09 10:28:10 -0400

    Updates

---
+++ precache-manifest.ab49bc695f4ee089717e551af8f6d602.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39566c49889bef022e2eb8bdff1b9d50",
+    "url": "/ScotttP/react-app-practice/index.html"
+  },
+  {
+    "revision": "9b2c867b0c1d38a51f1c",
+    "url": "/ScotttP/react-app-practice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "6007491b94cc8e3f5259",
+    "url": "/ScotttP/react-app-practice/static/js/2.09f9980d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ScotttP/react-app-practice/static/js/2.09f9980d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b2c867b0c1d38a51f1c",
+    "url": "/ScotttP/react-app-practice/static/js/main.c855bfed.chunk.js"
+  },
+  {
+    "revision": "53529796e042b9b44ad2",
+    "url": "/ScotttP/react-app-practice/static/js/runtime-main.0c5c68c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ScotttP/react-app-practice/static/media/logo.5d5d9eef.svg"
+  }
+]);
